b4dd467d160e15b864ee00933d26822a64f35e98
karthickkumar63826 <karthickkumar63826@gmail.com>
2026-03-20 14:41:57 +0530

    site-hosting-changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/shri-rahamandra/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
